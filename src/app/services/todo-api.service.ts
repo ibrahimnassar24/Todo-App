736680e86ade1eb5251f11ad40251b6dc309fe44
ibrahimnassar24 <74038100+ibrahimnassar24@users.noreply.github.com.>
2024-12-14 18:04:47 +0200

    services todo-api: create methods\n - createTodo\n- getSingleTodo\n- getAllTodos\n- updateTodo\n-removeTodo

--- src/app/services/todo-api.service.ts
+++ src/app/services/todo-api.service.ts
@@ -1,27 +1,98 @@
 import { Injectable, Inject } from '@angular/core';
-import { Todo } from '../state/todo/todo.model';
-import { collection, Firestore, getDocs } from 'firebase/firestore';
-import { from } from 'rxjs';
+import { Todo, TodoToUpdate } from '../state/todo/todo.model';
+import * as todosActions from "../state/todo/todo.actions";
+import {
+  addDoc,
+  collection,
+  CollectionReference,
+  deleteDoc,
+  doc,
+  Firestore,
+  getDoc,
+  getDocs,
+  onSnapshot,
+  query,
+  updateDoc,
+  where
+} from 'firebase/firestore';
 import { FirebaseService } from './firebase.service';
+import { AuthService } from './auth.service';
+import { Store } from '@ngrx/store';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoApiService {
-
+  firestore: Firestore;
+  todosCollection: CollectionReference;
+  
   constructor(
-    private  firebase: FirebaseService
-  ) { }
-
-  async getAllAsync() {
-    const coll = collection(this.firebase.firestore, "todos")
-    const snapshot = await getDocs(coll);
-    const data = snapshot.docs.map(d => d.data() as Todo);
-
-    return data;
+    private firebase: FirebaseService,
+    private authService: AuthService,
+    private store: Store
+  ) {
+    this.firestore = this.firebase.firestore;
+    this.todosCollection = collection(this.firestore, "todos");
   }
-  getAllTodos() {
-    const temp = this.getAllAsync();
-    return from(temp);
+
+  async getAllTodos(uid: string) {
+    try {
+      const q = query(
+        this.todosCollection,
+      where("ownerId", "==", uid)
+      );
+      const snapshot = await getDocs(q);
+      const data = snapshot.docs.map(d => ({ ...d.data() as Todo, id: d.id }));
+
+      return data;
+    } catch (e) {
+      throw e;
+    }
   }
+
+
+  async getSingleTodo(id: string) {
+    try {
+      const temp = doc(this.todosCollection, id);
+      const snapshot = await getDoc(temp);
+      return { ...snapshot.data() as Todo, id: snapshot.id };
+    }
+    catch (e) {
+      throw e;
+    }
+  }
+
+
+  async createTodo(todo: Todo) {
+    try {
+      const docref = await addDoc(
+        this.todosCollection,
+        todo
+      );
+    } catch (e) {
+      throw e;
+    }
+  }
+
+
+  async updateTodo(id: string, todoToUpdate: TodoToUpdate) {
+    try {
+      const todo = doc(this.todosCollection, id);
+      await updateDoc(todo, { ...todoToUpdate });
+    } catch (e) {
+      throw e;
+    }
+  }
+
+
+  async removeTodo(id: string) {
+    try {
+      const todo = doc(this.todosCollection, id);
+      await deleteDoc(todo);
+    } catch (e) {
+      throw e;
+    }
+  }
+
+
 }
