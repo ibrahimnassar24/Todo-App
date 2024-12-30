793ca807c955aed9af31c0f39c5286ceb3424538
ibrahim nassar <ebrahim24111990@gmail.com>
2024-12-30 17:54:07 +0200

    components signup-form: initial Ui

--- src/app/components/signup-form/signup-form.component.ts
+++ src/app/components/signup-form/signup-form.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
+import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import * as authActions from '../../state/auth/auth.actions';
+import { MatCard, MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatDividerModule } from "@angular/material/divider";
 
 @Component({
     selector: 'app-signup-form',
     imports: [
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatDividerModule
     ],
     templateUrl: './signup-form.component.html',
     styleUrl: './signup-form.component.scss'
@@ -27,13 +37,17 @@ export class SignupFormComponent {
       email: this.signUpFormState.value.email ?? "",
       password: this.signUpFormState.value.password ?? ""
     };
-
-    this.store.dispatch(authActions.signUp(temp));
+console.log(temp);
+    // this.store.dispatch(authActions.signUp(temp));
   }
 
-  allowSubmit() {
-    // return this.signUpFormState.value.password === this.signUpFormState.value.confirm;
-    return false
+  signUpUsingGoogle() {
+    this.store.dispatch(authActions.SignInWithGoogle())
   }
+
+  signUpUsingFacebook() {
+    this.store.dispatch(authActions.signInWithFacebook());
+  }
+
 
 }
