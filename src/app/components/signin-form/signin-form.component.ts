481ca391d55dc6c5634d475b60bd6e182a570077
ibrahim nassar <ebrahim24111990@gmail.com>
2024-12-30 17:28:56 +0200

    components signin-form: initial UI

--- src/app/components/signin-form/signin-form.component.ts
+++ src/app/components/signin-form/signin-form.component.ts
@@ -2,30 +2,53 @@ import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import * as authActions from '../../state/auth/auth.actions';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatDividerModule } from "@angular/material/divider";
+
+
 @Component({
-    selector: 'app-signin-form',
-    imports: [
-        ReactiveFormsModule
-    ],
-    templateUrl: './signin-form.component.html',
-    styleUrl: './signin-form.component.scss'
+  selector: 'app-signin-form',
+  imports: [
+    ReactiveFormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDividerModule
+  ],
+  templateUrl: './signin-form.component.html',
+  styleUrl: './signin-form.component.scss'
 })
 export class SigninFormComponent {
-  
+
   constructor(
     private store: Store
-  ) {}
+  ) { }
   signInFormState = new FormGroup({
     email: new FormControl(""),
     password: new FormControl("")
   });
 
   onSignIn(e: Event) {
+    e.preventDefault();
     const temp = {
       email: this.signInFormState.value.email as string,
       password: this.signInFormState.value.password as string
     };
-    
-    this.store.dispatch(authActions.signInWithEmailAndPassword( temp ));
+
+    this.store.dispatch(authActions.signInWithEmailAndPassword(temp));
+  }
+
+  signInUsingGoogle() {
+    console.log("sign in using google");
+    // this.store.dispatch(authActions.SignInWithGoogle());
+  }
+
+  signInUsingFacebook() {
+    console.log("sign in using facebook");
+    this.store.dispatch(authActions.signInWithFacebook());
   }
 }
