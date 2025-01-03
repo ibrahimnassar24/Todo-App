49021e68dcc768ec4988482e2268e4b31c4c1aa7
ibrahim nassar <ebrahim24111990@gmail.com>
2025-01-03 19:31:33 +0200

    components profile-menu: create + initialUI

---
+++ src/app/components/nav-bar/profile-menu/profile-menu.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatMenuModule } from "@angular/material/menu";
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import * as authActions from "../../../state/auth/auth.actions";
+import * as authSelectors from "../../../state/auth/auth.selectors";
+import { UserInfo } from 'firebase/auth';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-profile-menu',
+  imports: [
+    CommonModule,
+    RouterLink,
+    MatMenuModule,
+    MatButtonModule,
+    MatCardModule
+  ],
+  templateUrl: './profile-menu.component.html',
+  styleUrl: './profile-menu.component.scss'
+})
+export class ProfileMenuComponent {
+  photoUrl$: Observable<string | null | undefined>;
+  displayName$ : Observable<string | null | undefined>;
+  constructor(
+    private store: Store
+  ) {
+    this.photoUrl$ = this.store.select(authSelectors.selectUserPhotoUrl);
+    this.displayName$ = this.store.select(authSelectors.selectUserDisplayName);
+  }
+
+  signOut() {
+    this.store.dispatch(authActions.signOut());
+  }
+
+}
