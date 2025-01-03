8094b186b80ae11ac3c802c3f0c5c3b4e3503ba6
ibrahim nassar <ebrahim24111990@gmail.com>
2025-01-03 19:33:30 +0200

    components nav-bar: initial UI

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,15 +1,39 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatButtonModule } from "@angular/material/button"
+import { MatSlideToggleChange, MatSlideToggleModule } from "@angular/material/slide-toggle"
+import { MatTabsModule} from "@angular/material/tabs"
+import { ProfileMenuComponent } from './profile-menu/profile-menu.component';
 
 @Component({
     selector: 'app-nav-bar',
     imports: [
         RouterLink,
-        RouterLinkActive
+        RouterLinkActive,
+        MatButtonModule,
+        MatSlideToggleModule,
+        MatTabsModule,
+        ProfileMenuComponent
     ],
     templateUrl: './nav-bar.component.html',
     styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
 
+    activeLink = 0;
+
+    setActiveLink(index: number) {
+        this.activeLink = index;
+    }
+    
+    switchMode(e: MatSlideToggleChange) {
+
+        const lst = document.body.classList;
+        if (e.checked) {
+            lst.add("dark-mode");
+        } else {
+            lst.remove("dark-mode");
+        }
+
+    }
 }
